d0778275a210380c787b3a3d886d357c866b671c
zhengnianchao <zhengnianchao@live.com>
2019-11-11 18:33:49 +0800

    优化碰撞检测算法

--- main/circle/util.py
+++ main/circle/util.py
@@ -1,11 +1,13 @@
 # coding=utf-8
 def circle_in_rect(circle, rect):
     """判断圆形是否在矩形范围内，如果不在返回出了哪个方向"""
-    result = {'in': False,
-              'left': False,
-              'right': False,
-              'up': False,
-              'down': False}
+    result = {
+        'in': False,
+        'left': False,
+        'right': False,
+        'up': False,
+        'down': False
+    }
     if circle.x - circle.r >= rect.x and circle.x + circle.r <= rect.x + rect.width:
         if circle.y - circle.r >= rect.y and circle.y + circle.r <= rect.y + rect.height:
             result['in'] = True
